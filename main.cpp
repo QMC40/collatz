b7acfcdc46f38b7354cd2827af34d6d9e058a9d5
Aaron Fortner <afortner@islander.tamucc.edu>
2022-02-21 18:02:26 -0600

    works, still no print setup and no idea what is going to file from output

--- main.cpp
+++ main.cpp
@@ -5,19 +5,29 @@
 #include <cstdlib>
 #include <sys/fcntl.h>
 #include <string>
+#include <cstring>
+#include <cmath>
 
-void kidz();
+void collatz(int num, int Fd);
 
-void collatz(int num);
+int getNum(int Fd, int max);
 
-int getNum();
+int fileOpener(int fileSelector, int modeSelector = 1);
 
-int min, max;
+void fileClose(int Fd);
 
+void filePrinter(int Fd, int min, int max);
+
+enum ModeSelector {
+    READ, WRITE, READWRITE
+};
+enum FileSelector {
+    INPUT, OUTPUT
+};
 
 int main(int argc, char *argv[]) {
 
-    int n, record;
+    int n = 0, min = 0, max = 0, myNumber = 0;
 
     //getting entering args into vars
     if (argc < 3) {
@@ -30,85 +40,188 @@ int main(int argc, char *argv[]) {
     if (n <= 0 || min <= 0 || max <= 0) {
         printf("command line arguments must be positive integers, please try again.\n");
         return 1;
+    } else if (n > 10) {
+        printf("number of processes cannot exceed 10, please try again.\n");
     }
-    printf("n: %d min: %d max: %d\n", n, min, max);
 
-    pid_t parentPid = getpid(), childPid = 0;
+    //collecting job numbers for individual processes
+    int range = (max+1 - min),
+            leaders = (int) (ceil((double) n / 2)),
+            followers = n - leaders,
+            smShare = range / 3,
+            bigShare = range - smShare,
+            bigBite = (int)(ceil((double)bigShare / leaders)),
+            smBite = smShare / followers;
 
-    printf("parent is: %d\n", parentPid);
+    //testing output to check math
+//    printf("n: %d min: %d max: %d\n", n, min, max);
+//    printf("range: %d leaders: %d \n"
+//           "smShare: %d bigShare: %d \n"
+//           "bigBite: %d smBite: %d total: %d\n",
+//           range, leaders, smShare, bigShare,
+//           bigBite, smBite, (leaders * bigBite) + (followers * smBite));
 
-    //open file to write results too
-    if ((record = open("data.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1) {
-        perror("data file failed to open");
-    }
+    //creates / opens file for number pool / results and populated it
+    int numberPool = 0;
+    numberPool = fileOpener(INPUT, READWRITE);
 
     //write range into file to be accessed by child processes
-    int buff;
     for (int i = min; i <= max; i++) {
-        write(record, &i, 4);
-        read(record, &buff, 4);
+        if (write(numberPool, &i, 4) == -1) {
+            perror("write failed");
+            exit(EXIT_FAILURE);
+        };
     }
-    //rewind offset to first byte of file, could also just close and reopen file...
-    lseek(record, 0, SEEK_SET);
 
-//    for (int i = min; i <= max; i++) {
-//        read(record, &buff, 4);
-//        printf("buff = %d\n", buff);
-//    }
+    //put number pool file in read only mode and rewind it back to start
+    if(fcntl(numberPool,F_SETFL,O_RDONLY) == -1) {
+        perror("unable to set file to read only mode\n");
+        exit(EXIT_FAILURE);
+    }    if(lseek(numberPool,0,SEEK_SET) == -1) {
+        perror("unable to set file offset back to start of file\n");
+        exit(EXIT_FAILURE);
+    }
 
+    //creates / opens file for sequence results from child processes
+    int record = fileOpener(OUTPUT,WRITE);
 
+    pid_t parentPid = getpid();
+
+    printf("Creating %d processes:\n", n);
+
+    //fork child processes to do sequences
+    auto *children = new pid_t[n];   //dynamic array to hold pids of children forked off
     for (int i = 0; i < n; i++) {
-        if ((childPid = fork()) <= 0) {
-            if (childPid == -1) {
-                printf("fork failed!\n");
-                return 1;
+        if ((children[i] = fork()) <= 0) {
+            if (children[i] == -1) {
+                perror("fork failed!");
+                exit(EXIT_FAILURE);
             } else {
-//                kidz();
-                break;
+                myNumber = i + 1;
             }
+            break;
         }
     }
-//    dup2(oldOut,STDOUT_FILENO);
-//    while (wait(nullptr) >= 0) {}
-//
-//    if (getpid() == parentPid) {
-//        printf("Parent %d process is completed.\n", getpid());
-//    }
 
-    if ((record = close(record)) == -1) {
-        perror("data file failed to close");
+    //screen children out and set their task count "stop"
+    if (myNumber > 0) {
+        int stop = 0, low = max, high = min;
+        if (myNumber <= leaders) {
+            stop = bigBite;
+        } else {
+            stop = smBite;
+        }
+
+
+        //children do their assigned number of sequences
+        for (int i = 0; i < stop; i++) {
+            int num;
+            num = getNum(numberPool, max);
+            low = (num < low) ? num : low;
+            high = (high < num) ? num : high;
+            if (num > 1 && num <= max) {
+                collatz(num, record);
+            }
+
+        }
+        printf("I am child number %ld, my parent is %ld, "
+               "I computed the Collatz sequence for numbers from %d to %d\n",
+               (long) getpid(), (long) getppid(), low, high);
     }
 
+    //close
+    fileClose(numberPool);
+
+    //cutoff for children
+    if (myNumber > 0) { exit(0); }
+
+    //parent waits for children to finish
+    while (wait(nullptr) >= 0) {}
+
+    if (getpid() == parentPid) {
+        printf("Parent %ld process is completed.\n", (long) getpid());
+//        for (int i = 0; i < n; i++) {
+//            printf("%d\n", children[i]);
+//        }
+        filePrinter(record,0,25);
+    }
     return 0;
 }
 
-void kidz() {
-    pid_t myID = getpid();
-    //segregate parent from children
-    if (myID != 0) {
-        printf("Child %d is working...\n", myID);
-        collatz(getNum());
-        printf("\n");
-        printf("Child %d process is completed.\n", myID);
-    }
-}
-
-void collatz(int num) {
-    /*this is for check and print all positive
-      numbers will finally reach 1*/
-    printf("collatz\n");
-    while (num != 1) {
+//conducts collatz sequence with supplied seed value and writes it to file / console
+void collatz(int num, int Fd) {
+//    printf("%d ", num);
+    write(Fd,&num,4);
+    while (num > 1) {
         if (num % 2 == 0) {
             num = num / 2;
         } else if (num % 2 == 1) {
             num = 3 * (num) + 1;
         }
-        printf("%d ", num);
+        write(Fd, &num, 4);
+        write(Fd," ",1);
+//        printf("%d ", num);
+    }
+    write(Fd,"\0",1);
+//    printf("\n");
+}
+
+//collects a seed number off the script file and returns it
+int getNum(int Fd, int max) {
+    int buff;
+    read(Fd, &buff, 4);
+    return buff;
+}
+
+//opens file and checks for error
+int fileOpener(int fileSelector, int modeSelector) {
+    int Fd, openFlags, filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+                                   S_IROTH | S_IWOTH; /* rw-rw-rw- */
+    const char *fileName = (fileSelector) ? "results_Childpid_Parentpid.dat"
+                                            : "number_Pool.dat";
+    switch (modeSelector) {
+        case READWRITE: {
+            openFlags =  O_RDWR | O_CREAT | O_TRUNC;
+            break;
+        }
+        case WRITE: {
+            openFlags = O_WRONLY;
+            break;
+        }
+        case READ: {
+            openFlags = O_RDONLY;
+            break;
+        }
+        default: {
+            openFlags = O_RDONLY;
+            break;
+        }
+    }
+    if ((Fd = open(fileName, openFlags, filePerms)) == -1) {
+        perror("data file failed to open");
+        printf("File: %s", fileName);
+        exit(EXIT_FAILURE);
+    }
+    return Fd;
+}
+
+//closes file and checks for error
+void fileClose(int Fd) {
+    if ((close(Fd)) == -1) {
+        perror("data file failed to close");
+        exit(EXIT_FAILURE);
     }
 }
 
-int getNum() {
-    printf("getNum min: %d\n", min);
-    while (min <= max) { return min++; }
-    return -1;
+//utility for printing out file contents for testing
+void filePrinter(int Fd, int min, int max) {
+    int buff = 0;
+    ssize_t  numRead = 0;
+    while((numRead = read(Fd, &buff, 4)) > 0) {
+        if(write(STDOUT_FILENO,&buff,numRead) != numRead) {
+            perror("unable to write whole buffer");
+        }
+        printf("buff = %d\n", buff);
+        if(numRead == -1) { perror("file read error"); }
+    }
 }
