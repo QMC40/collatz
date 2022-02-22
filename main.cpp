63b8a47994b01dfbaebd6df06acc4e42968f5d71
Aaron Fortner <afortner@islander.tamucc.edu>
2022-02-21 21:07:12 -0600

    just can't get the pid of the terminated children

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ void collatz(int num, int Fd);
 
 int getNum(int Fd);
 
-int fileOpener(const std::string& myFile, int modeSelector = 1);
+int fileOpener(const std::string &myFile, int modeSelector = 1);
 
-std::string nameMaker(pid_t subject,pid_t parent);
+std::string nameMaker(pid_t subject, pid_t parent);
 
 void fileClose(int Fd);
+
+void resultDispenser(pid_t children[],int n);
 
 void filePrinter(int Fd);
 
@@ -26,7 +28,7 @@ enum ModeSelector {
 
 int main(int argc, char *argv[]) {
 
-    int n = 0, min = 0, max = 0, myNumber = 0;
+    int n = 1, min = 0, max = 0, myNumber = 0;
 
     pid_t parentPid = getpid();
 
@@ -46,14 +48,18 @@ int main(int argc, char *argv[]) {
     }
 
     //collecting job numbers for individual processes
-    int range = (max+1 - min),
-            leaders = (int) (ceil((double) n / 2)),
-            followers = n - leaders,
-            smShare = range / 3,
-            bigShare = range - smShare,
-            bigBite = (int)(ceil((double)bigShare / leaders)),
-            smBite = smShare / followers;
-
+    int range = 1, leaders = 1, followers = 1
+    , smShare = 0, bigShare = 1, smBite = 0, bigBite = 1;
+    //check if single process requested, otherwise do the math
+    if (n > 1) {
+        range = (max + 1 - min),
+        leaders = (int) (ceil((double) n / 2)),
+        followers = n - leaders,
+        smShare = range / 3,
+        bigShare = range - smShare,
+        bigBite = (int) (ceil((double) bigShare / leaders)),
+        smBite = smShare / followers;
+    }
     //testing output to check math
 //    printf("n: %d min: %d max: %d\n", n, min, max);
 //    printf("range: %d leaders: %d \n"
@@ -64,7 +70,7 @@ int main(int argc, char *argv[]) {
 
     //creates / opens file for number pool / results and populated it
     int numberPool = 0;
-    numberPool = fileOpener("number_Pool.dat",READWRITE);
+    numberPool = fileOpener("number_Pool.dat", READWRITE);
 
     //write range into file to be accessed by child processes
     for (int i = min; i <= max; i++) {
@@ -75,17 +81,14 @@ int main(int argc, char *argv[]) {
     }
 
     //put number pool file in read only mode and rewind it back to start
-    if(fcntl(numberPool,F_SETFL,O_RDONLY) == -1) {
+    if (fcntl(numberPool, F_SETFL, O_RDONLY) == -1) {
         perror("unable to set file to read only mode\n");
         exit(EXIT_FAILURE);
-    }    if(lseek(numberPool,0,SEEK_SET) == -1) {
+    }
+    if (lseek(numberPool, 0, SEEK_SET) == -1) {
         perror("unable to set file offset back to start of file\n");
         exit(EXIT_FAILURE);
     }
-
-
-
-
 
     printf("Creating %d processes:\n", n);
 
@@ -103,17 +106,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
     //screen children out and set their task count "stop"
     if (myNumber > 0) {
         //creates / opens file for sequence results from child processes
-        int record = fileOpener(nameMaker((long)getpid(),(long)getppid()),
+        int record = fileOpener(nameMaker((long) getpid(), (long) getppid()),
                                 READWRITE);
 
         //set task list
         int stop = 0, low = max, high = min;
         if (myNumber <= leaders) {
-            stop = bigBite;
+            stop = (bigBite > 0) ? bigBite : 1;
         } else {
             stop = smBite;
         }
@@ -138,27 +140,33 @@ int main(int argc, char *argv[]) {
     //close file used for issuing numbers to children
     fileClose(numberPool);
 
-    //cutoff for children
-    if (myNumber > 0) { exit(0); }
-
+    if (getpid() == parentPid) {
     //parent waits for children to finish
     while (wait(nullptr) >= 0) {}
 
-    if (getpid() == parentPid) {
-        for (int i = 0; i < n; i++) {
-            std::string name = nameMaker(children[i],getpid());
-            int childFile = fileOpener(name,READ);
-            filePrinter(childFile);
-            fileClose(childFile);
+    printf("\nProcesses completing: \n");
+
+        int tails = n;
+        int status;
+        pid_t pid;
+        while (tails > 0) {
+            pid = wait(&status);
+            printf("Process that has PID %ld exited with status 0x%x.\n", (long) pid, status);
+            --tails;
         }
+        resultDispenser(children,n);
     }
+
+
+
+
     return 0;
 }
 
 //conducts collatz sequence with supplied seed value and writes it to file / console
 void collatz(int num, int Fd) {
 //    printf("%d ", num);
-    write(Fd,&num,4);
+    write(Fd, &num, 4);
     while (num > 1) {
         if (num % 2 == 0) {
             num = num / 2;
@@ -166,10 +174,10 @@ void collatz(int num, int Fd) {
             num = 3 * (num) + 1;
         }
         write(Fd, &num, 4);
-        write(Fd," ",1);
+//        write(Fd," ",4);
 //        printf("%d ", num);
     }
-    write(Fd,"\0",1);
+//    write(Fd,"\0",4);
 //    printf("\n");
 }
 
@@ -181,19 +189,19 @@ int getNum(int Fd) {
 }
 
 //make name file out of pid supplied
-std::string nameMaker(pid_t subject,pid_t parent) {
-    return "results_"+std::to_string(subject)+"_"+
-                         std::to_string(parent);
+std::string nameMaker(pid_t subject, pid_t parent) {
+    return "results_" + std::to_string(subject) + "_" +
+           std::to_string(parent);
 }
 
 //opens file and checks for error
-int fileOpener(const std::string& myFile,int modeSelector) {
+int fileOpener(const std::string &myFile, int modeSelector) {
     int Fd, openFlags, filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
                                    S_IROTH | S_IWOTH; /* rw-rw-rw- */
 
     switch (modeSelector) {
         case READWRITE: {
-            openFlags =  O_RDWR | O_CREAT | O_TRUNC;
+            openFlags = O_RDWR | O_CREAT | O_TRUNC;
             break;
         }
         case WRITE: {
@@ -225,15 +233,22 @@ void fileClose(int Fd) {
     }
 }
 
+void resultDispenser(pid_t children[],int n) {
+    for (int i = 0; i < n; i++) {
+        std::string name = nameMaker(children[i], getpid());
+        int childFile = fileOpener(name, READ);
+        filePrinter(childFile);
+        fileClose(childFile);
+    }
+}
+
 //utility for printing out file contents for testing
 void filePrinter(int Fd) {
     int buff = 0;
-    ssize_t  numRead = 0;
-    while((numRead = read(Fd, &buff, 4)) > 0) {
-        if(write(STDOUT_FILENO,&buff,numRead) != numRead) {
-            perror("unable to write whole buffer");
-        }
-        printf("buff = %d\n", buff);
-        if(numRead == -1) { perror("file read error"); }
+    ssize_t numRead = 0;
+    while ((numRead = read(Fd, &buff, 4)) > 0) {
+        if (numRead == -1) { perror("file read error"); }
+        if (buff != 1) { printf("%d ", buff); }
+        else { printf("1\n"); }
     }
 }
